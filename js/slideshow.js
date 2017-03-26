82fe8712b32054bfda73a2b9b4278b87a49cd700
Christian Strait <impresently@gmail.com>
2017-03-26 17:25:12 -0500

    Add slider control function

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,28 +1,86 @@
-window.onload = function ()
-{
 
-	var slideArray = ["A", "B", "C", "D", "E"];
+var $slides = $('#slidesContainer div');
+var slidesArr = [];
+slidesArr[0] = $('#a');
+slidesArr[1] = $('#b');
+slidesArr[2] = $('#c');
+slidesArr[3] = $('#d');
+slidesArr[4] = $('#e');
 
-var slideTimer = 100
+var currentSlideIndex = 0;
+var slideTimer = 1000
+var playInterval = setInterval(autoplay,slideTimer);
+var randomColorIndex = 0;
+var pauseBtn = document.getElementById('pause');
+var playBtn = document.getElementById('play');
+var nextBtn = document.getElementById('next');
+var previousBtn = document.getElementById('previous');
 
-var activeSlide = 0;
+pauseBtn.addEventListener("click", pauseSlider);
+playBtn.addEventListener("click", playSlider);
+nextBtn.addEventListener("click", nextSlide);
+previousBtn.addEventListener("click", previousSlide);
 
-var randomColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 
-var slide = document.getElementById("slide");
-    slide.className="activeSlide";
-
-var slideInterval = setInterval(next,slideTimer);
-
-function next() {
-  slide.innerHTML = slideArray[activeSlide];
-  activeSlide++;
-  if (activeSlide > (slideArray.length - 1)) {
-    activeSlide = 0;
-  }
-  slide.style.backgroundColor=randomColor;
+function generateRandomColor() {
+    return '#'+Math.floor(Math.random()*16777215).toString(16);
 }
 
+for(var i=0; i<slidesArr.length; i++) {
+	var randomColor = slidesArr[randomColorIndex];
+	randomColorIndex++;
+  	randomColor.css('background-color', generateRandomColor());
+};
+
+function showCurrentSlide () {
+    $slides.hide();
+    slidesArr[currentSlideIndex].show();
+};
+
+function autoplay() {
+    currentSlideIndex++;
+    if(currentSlideIndex === slidesArr.length) {
+        currentSlideIndex = 0;
+    };
+	showCurrentSlide();
+};
+
+function pauseSlider() {
+	clearInterval(playInterval);
+};
+
+function playSlider() {
+	playInterval = setInterval(autoplay,slideTimer);
+};
+
+function nextSlide () {
+	currentSlideIndex++;
+	if(currentSlideIndex === slidesArr.length) {
+        currentSlideIndex = 0;
+    };
+	showCurrentSlide();
+};
+
+function previousSlide () {
+	if(currentSlideIndex === 0) {
+        currentSlideIndex = slidesArr.length;
+    };
+	currentSlideIndex--;
+	showCurrentSlide();
+};
 
 
-}
+
+
+
+	
+	
+
+
+
+
+
+
+
+
+
